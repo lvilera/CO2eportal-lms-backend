e6ff952e884c1575872015d9d84b200ef821d9ee
Belayet Riad <belayetriadbd@gmail.com>
2025-11-12 23:11:32 -0800

    update user and fix guard

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,31 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '../src/app.module';
+import { AppModule } from 'src/app.module';
 
-let cachedServer: any;
+let server: any;
 
 export default async function handler(req: any, res: any) {
-  if (!cachedServer) {
-    const app = await NestFactory.create(AppModule, {
-      logger: ['error', 'warn'],
-    });
-    app.useGlobalPipes(
-      new ValidationPipe({ whitelist: true, transform: true }),
-    );
-    // Do NOT call app.listen() on Vercel
-    await app.init();
-    cachedServer = app.getHttpAdapter().getInstance();
+  try {
+    if (!server) {
+      const app = await NestFactory.create(AppModule, {
+        logger: ['error', 'warn'],
+      });
+
+      app.useGlobalPipes(
+        new ValidationPipe({
+          whitelist: true,
+          transform: true,
+        }),
+      );
+
+      await app.init();
+      server = app.getHttpAdapter().getInstance();
+    }
+
+    return server(req, res);
+  } catch (e) {
+    console.error('Lambda error:', e);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
   }
-  return cachedServer(req, res);
 }
